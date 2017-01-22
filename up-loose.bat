ce3baf8e2cdd622f27c63734fca9410a650e9b00
digitalpartisan <digital.partisan.86@gmail.com>
2017-01-21 17:29:21 -0800

    Added packed files and .bat utilities

---
+++ up-loose.bat
@@ -0,0 +1,2 @@
+mklink /j "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\CheatBunkerDLC04" "K:\FO4 Mods\CheatBunkerNukaWorld\Data\Scripts\CheatBunkerDLC04"
+mklink /j "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\Source\User\CheatBunkerDLC04" "K:\FO4 Mods\CheatBunkerNukaWorld\Data\Scripts\Source\User\CheatBunkerDLC04"
---
+++ up-packed.bat
@@ -0,0 +1,1 @@
+mklink /h "K:\Steam\steamapps\common\Fallout 4\Data\CheatBunkerNukaWorld - Main.ba2" "K:\FO4 Mods\CheatBunkerNukaWorld\Data\CheatBunkerNukaWorld - Main.ba2"
