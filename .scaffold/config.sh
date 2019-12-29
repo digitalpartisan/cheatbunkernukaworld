18ae268a800f7e4898f3eeaa9dc06eb1c48805e4
digitalpartisan <digital.partisan.86@gmail.com>
2019-12-28 16:26:54 -0800

    Updated scaffold config

--- .scaffold/config.sh
+++ .scaffold/config.sh
@@ -1,4 +1,2 @@
-SCAFFOLD_MOD_NAME="CheatBunkerNukaWorld"
-SCAFFOLD_MOD_GAME_PATH="/k/Steam/steamapps/common/Fallout 4"
-SCAFFOLD_MOD_DELIVERABLES_PATH="/k/FO4 Modding/Packaging"
-SCAFFOLD_MOD_DEPENDENCIES=""
+SCAFFOLD_MOD="CheatBunkerNukaWorld"
+SCAFFOLD_GAME="fallout4"
